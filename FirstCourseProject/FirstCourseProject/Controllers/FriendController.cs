30277091045c8d52ff851a9aee9e2abce4763ead
Ahmadreza <ahmadrezam@systemgroup.net>
2024-04-26 00:52:41 +0330

    Adding list of friends

---
+++ FirstCourseProject/FirstCourseProject/Controllers/FriendController.cs
@@ -0,0 +1,30 @@
+﻿using FirstCourseProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstCourseProject.Controllers
+{
+    public class FriendController : Controller
+    {
+        private List<FriendModel> FriendsList = new();
+
+        public FriendController()
+        {
+            FriendsList.Add(new FriendModel(1, "Abolfazl Norouzi", "09192224455", ""));
+            FriendsList.Add(new FriendModel(2, "Amirreza Mozaffary", "09194444455", ""));
+            FriendsList.Add(new FriendModel(3, "Ali Motamedi", "09194534455", ""));
+            FriendsList.Add(new FriendModel(4, "Farid Mojaddam", "09192344455", ""));
+            FriendsList.Add(new FriendModel(5, "Amin Afrasiabi", "09191239455", ""));
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Friends = FriendsList;
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirstCourseProject/FirstCourseProject/Models/FriendModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstCourseProject.Models
+{
+    public class FriendModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Avatar { get; set; }
+
+        public FriendModel(int id, string name, string phoneNumber, string avatar)
+        {
+            Id = id;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Avatar = avatar;
+        }
+    }
+}
