b8adae567dc41906659ec9d36a3ff271f24b5036
Ahmadreza <ahmadrezam@systemgroup.net>
2024-04-26 14:21:09 +0330

    add detail page ( Project Finished )

--- FirstCourseProject/FirstCourseProject/Controllers/FriendController.cs
+++ FirstCourseProject/FirstCourseProject/Controllers/FriendController.cs
@@ -9,11 +9,17 @@ namespace FirstCourseProject.Controllers
 
         public FriendController()
         {
-            FriendsList.Add(new FriendModel(1, "Abolfazl Norouzi", "09192224455", ""));
-            FriendsList.Add(new FriendModel(2, "Amirreza Mozaffary", "09194444455", ""));
-            FriendsList.Add(new FriendModel(3, "Ali Motamedi", "09194534455", ""));
-            FriendsList.Add(new FriendModel(4, "Farid Mojaddam", "09192344455", ""));
-            FriendsList.Add(new FriendModel(5, "Amin Afrasiabi", "09191239455", ""));
+            InitFriendsList();
+        }
+
+        private void InitFriendsList()
+        {
+            FriendsList.Add(new FriendModel(1, "Abolfazl Norouzi", "0919-2224455", "/img/1.jpg"));
+            FriendsList.Add(new FriendModel(2, "Amirreza MozaffaryDehno", "0919-4444455", "/img/2.jpg"));
+            FriendsList.Add(new FriendModel(3, "Ali Motamedi", "0919-4534455", "/img/3.jpg"));
+            FriendsList.Add(new FriendModel(4, "Farid Mojaddam", "0919-2344455", "/img/4.jpg"));
+            FriendsList.Add(new FriendModel(5, "Amin Afrasiabi", "0919-1239455", "/img/5.jpg"));
+
         }
 
         public IActionResult List()
@@ -22,8 +28,10 @@ namespace FirstCourseProject.Controllers
             return View();
         }
 
-        public IActionResult Details()
+        [Route("Friend/Details/{id:int}")]
+        public IActionResult Details(int id)
         {
+            ViewBag.CurrentFriend = FriendsList.Find(f => f.Id == id);
             return View();
         }
     }
