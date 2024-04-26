991800547a1cafbc9a8427a00776e0c0dd2fcb25
Ahmadreza <ahmadrezam@systemgroup.net>
2024-04-26 20:44:13 +0330

    Add login page

---
+++ Rooydaad.Web/Rooydaad.Web/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rooydaad.Web.Models;
+
+namespace Rooydaad.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Credentials credentials)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "You logged in ;)";
+                return View();
+            }
+
+            ViewBag.Message = string.Empty;
+            return View(credentials);
+        }
+    }
+}
---
+++ Rooydaad.Web/Rooydaad.Web/Models/Credentials.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rooydaad.Web.Models
+{
+    public class Credentials
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email address", Prompt = "example@domain.com")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
